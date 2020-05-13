c140688654a47e7b58a5098e9ad5e73341928b4d
Wasgo097 <patrpak906@student.polsl.pl>
2020-05-13 20:38:43 +0200

    during routing

--- src/app/one-hero/one-hero.component.ts
+++ src/app/one-hero/one-hero.component.ts
@@ -1,16 +1,23 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { Hero } from '../hero';
-
+import { ActivatedRoute } from '@angular/router';
+import { HeroService } from '../service/serv.service';
 @Component({
   selector: 'app-one-hero',
   templateUrl: './one-hero.component.html',
   styleUrls: ['./one-hero.component.css']
 })
 export class OneHeroComponent implements OnInit {
+  private id:number;
   @Input()
   public hero:Hero;
-  constructor() { }
+  constructor(private route: ActivatedRoute,private service:HeroService) { }
   ngOnInit(): void {
+    this.route.paramMap
+      .subscribe(params => {
+        this.id = +params.get('id');
+      });
+      this.service.getHero(this.id).subscribe(hero=>this.hero=hero);
   }
 
 }
