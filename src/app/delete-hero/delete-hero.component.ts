b01659cd0aa6fa97bbe26b8f9b46c8b665a2fc88
Wasgo097 <patrpak906@student.polsl.pl>
2020-05-15 16:31:59 +0200

    app v2.0

---
+++ src/app/delete-hero/delete-hero.component.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { HeroService } from '../service/serv.service';
+@Component({
+  selector: 'app-delete-hero',
+  templateUrl: './delete-hero.component.html',
+  styleUrls: ['./delete-hero.component.css']
+})
+export class DeleteHeroComponent implements OnInit {
+  private id:number;
+  public success=false;
+  constructor(private route: ActivatedRoute,private service:HeroService) { }
+  ngOnInit(): void {
+    this.route.paramMap.subscribe(params => {this.id = +params.get('id');});
+    //this.service.getHero(this.id).subscribe(hero=>{this.hero=hero;this.load=true;});
+    this.service.deleteHero(this.id).subscribe(result=>this.success=result);
+  }
+}
