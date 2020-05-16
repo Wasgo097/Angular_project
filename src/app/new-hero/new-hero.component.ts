7170e032273f42f5e84a770a8e9e6c62b3bd25e0
Wasgo097 <patrpak906@student.polsl.pl>
2020-05-16 13:37:13 +0200

    forms 1.1

--- src/app/new-hero/new-hero.component.ts
+++ src/app/new-hero/new-hero.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormGroup, FormBuilder, FormControl} from '@angular/forms';
+import { FormGroup, FormBuilder, FormControl } from '@angular/forms';
 import { Hero } from '../hero';
 import { HeroService } from '../service/serv.service';
 @Component({
@@ -8,22 +8,22 @@ import { HeroService } from '../service/serv.service';
   styleUrls: ['./new-hero.component.css']
 })
 export class NewHeroComponent implements OnInit {
-  public form:FormGroup
-  constructor(private fb:FormBuilder,private service:HeroService) {
-    this.form=fb.group({
-        id: new FormControl(null),
-        nick: new FormControl(null),
-        img:new FormControl(null),
-        prof:new FormControl(null)
+  public form: FormGroup
+  constructor(private fb: FormBuilder, private service: HeroService) {
+    this.form = fb.group({
+      id: new FormControl(null),
+      nick: new FormControl(null),
+      img: new FormControl(null),
+      prof: new FormControl(null)
     });
   }
   ngOnInit(): void {
   }
-  onSubmit(){
-    let newHero=this.form.value as Hero;
+  onSubmit() {
+    let newHero = this.form.value as Hero;
     //let temp=newHero.img;
     //newHero.img="../../assets/"+temp;
-    this.service.addHero(newHero).subscribe(result=>{console.log(result)});
+    this.service.addHero(newHero).subscribe(result => { console.log(result) });
     console.log(newHero);
   }
 }
