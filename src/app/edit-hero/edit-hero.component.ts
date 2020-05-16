3f3fed30b930e04f0921025e43301d71a2f22d80
Wasgo097 <patrpak906@student.polsl.pl>
2020-05-16 14:08:51 +0200

    App v1.1

--- src/app/edit-hero/edit-hero.component.ts
+++ src/app/edit-hero/edit-hero.component.ts
@@ -11,6 +11,7 @@ import { FormBuilder, FormGroup, FormControl } from '@angular/forms';
 export class EditHeroComponent implements OnInit {
   public form: FormGroup
   public id: number;
+  public load: boolean = false;
   public hero?: Hero;
   constructor(private fb: FormBuilder, private route: ActivatedRoute, private service: HeroService) {
     this.form = this.fb.group({
@@ -18,24 +19,31 @@ export class EditHeroComponent implements OnInit {
       // nick: new FormControl(this.hero.nick),
       // img: new FormControl(this.hero.img),
       // prof: new FormControl(this.hero.prof)
-        id: new FormControl(null),
-        nick: new FormControl(null),
-        img:new FormControl(null),
-        prof:new FormControl(null)
+      id: new FormControl(null),
+      nick: new FormControl(null),
+      img: new FormControl(null),
+      prof: new FormControl(null)
     });
   }
   ngOnInit(): void {
     this.route.paramMap.subscribe(params => { this.id = +params.get('id'); });
-    this.service.getHero(this.id).subscribe(hero => {this.hero = hero;console.log(this.hero)});
+    this.service.getHero(this.id).subscribe(hero => {
+    this.hero = hero; console.log(this.hero);
+      this.load = true;
+      this.form.controls["id"].setValue(this.hero.id);
+      this.form.controls["nick"].setValue(this.hero.nick);
+      this.form.controls["img"].setValue(this.hero.img);
+      this.form.controls["prof"].setValue(this.hero.prof);
+    });
   }
   onSubmit() {
     let newHero = this.form.value as Hero;
     console.log("Edycja");
-    if(newHero.id==null)newHero.id=this.hero.id;
-    if(newHero.nick==null)newHero.nick=this.hero.nick;
-    if(newHero.img==null)newHero.img=this.hero.img;
-    if(newHero.prof==null)newHero.prof=this.hero.prof;
+    if (newHero.id == null) newHero.id = this.hero.id;
+    if (newHero.nick == null) newHero.nick = this.hero.nick;
+    if (newHero.img == null) newHero.img = this.hero.img;
+    if (newHero.prof == null) newHero.prof = this.hero.prof;
     console.log(newHero);
-    this.service.updateHero(this.id, newHero).subscribe(result=>{console.log(result)});
+    this.service.updateHero(this.id, newHero).subscribe(result => { console.log(result) });
   }
 }
